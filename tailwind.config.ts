e584ff988fb734f68d952394e6527d4b98c7173c
Weslem Vitor <playn467@gmail.com>
2025-01-11 16:18:47 -0300

    :tada: first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,25 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        green: {
+          500: "#249B3E",
+          800: "#397947",
+        },
+        gray: {
+          100: "#F5F5F5",
+          300: "#666666",
+          800: "#2E2E2E",
+        },
+        blue: {
+          100: "#F3F9FB",
+          500: "#008ECC",
+        },
+        red: {
+          500: "#FF4242",
+        },
+        orange: {
+          800: "#F89828",
+        },
       },
     },
   },
